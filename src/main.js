ccba1c1a0bc14fb51a85ca87df12aa8cb447cf92
Phanumatlaloed <phanumat.la.65@ubu.ac.th>
2026-02-23 12:48:57 +0700

    upload full portfolio website

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import "./src/month1.css";
+import "./src/month2.css";
+import "./src/month3.css";
+import "./src/month4.css";
+import "./src/contact.css";
+import "./src/experiences.css";
+
+
+document.querySelector("#year")?.append(new Date().getFullYear());
